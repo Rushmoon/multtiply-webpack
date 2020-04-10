bab1904ac9ed86298dd2ff81b51f79f340ee743f
zhouhdf <zhouhdf@yonyou.com>
2020-04-10 17:03:30 +0800

    添加小练习（未完善）

---
+++ src/applet/DotaExcesices/Ker/main/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+
+class Invoker extends React.Component {
+    constructor(){
+        super()
+
+    }
+    render() {
+        return {
+
+        }
+    }
+}
