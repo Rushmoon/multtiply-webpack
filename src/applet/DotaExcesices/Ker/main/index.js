aa48928f0c45b6fd09de720a271b31c3283203e8
LoveAsuka <hailanzhd@sina.com>
2020-04-14 08:14:10 +0800

    资源提交

--- src/applet/DotaExcesices/Ker/main/index.js
+++ src/applet/DotaExcesices/Ker/main/index.js
@@ -1,14 +1,26 @@
 import React from 'react'
 import ReactDom from 'react-dom'
+import Skill from '../skill'
+import ReactTree from "../../../simpleTree/tree/container";
 
 class Invoker extends React.Component {
     constructor(){
-        super()
-
-    }
-    render() {
-        return {
-
+        super();
+        this.state = {
+            Q:'ice',
+            W:'thunder',
+            E:'fire',
+            firstSkill:null,
+            secondSkill:null
         }
     }
+
+    render() {
+        return
+            <Skill {...this.props} />
+
+    }
 }
+ReactDom.render((
+    <Invoker />
+),document.getElementById('app'))
