9dafb54d48439cf26840cdf10225024b8ab6133b
zhouhdf <zhouhdf@yonyou.com>
2020-10-12 16:52:56 +0800

    promise和订阅相关未完成

--- src/applet/licodeExpricise/codeJumper/arroperator/快速排序.js
+++ src/applet/licodeExpricise/codeJumper/arroperator/快速排序.js
@@ -1,28 +1,32 @@
 function hurrySort(start,end,content) {
+
+    if (start >= end) {
+        return content
+    }
+
     let i = start;
     let j = end;
     let temp = content[start];
-    let comp;
-    if(i<j){
-        while(i<j){
-            while (content[i] <= temp && i<j){
-                i++
-            }
-            while (content[j] >= temp && i<j){
-                j++
-            }
-            if(i<j){
-                comp = content[i];
-                content[i] = content[j];
-                content[j] = comp;
-            }
-        }
-        console.log(hurrySort(start+1,i-1,content),temp,hurrySort(i,end,content))
-        // return hurrySort(start+1,i-1,content).concat([temp]).concat(hurrySort(i,end,content))
-    }else {
-        return content[start]
-    }
-}
 
+    while (i < j) {
+        while (content[j] >= temp && i < j) {
+            j--
+        }
+        while (content[i] <= temp && i < j) {
+            i++
+        }
+
+        if (i < j) {
+            let comp = content[i];
+            content[i] = content[j];
+            content[j] = comp;
+        }
+    }
+    content[start] = content[i];
+    content[i] = temp;
+    hurrySort(start, i - 1, content);
+    hurrySort(i+1, end, content);
+    return content;
+}
 let arr = [3,1,4,1,5,9,2,6,5,3,5,7,9];
 console.log(hurrySort(0,arr.length-1,arr));
